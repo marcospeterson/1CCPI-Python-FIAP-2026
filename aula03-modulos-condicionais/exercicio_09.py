292b481dd48f9053e0d4a11114b1389172203163
marcospeterson <marcospetersonmartins@gmail.com>
2026-04-13 10:27:22 -0300

    terminando os exercicios

--- aula03-modulos-condicionais/exercicio_09.py
+++ aula03-modulos-condicionais/exercicio_09.py
@@ -1,27 +1,33 @@
-'''cod_estado = int(input("Digite o código do estado (1 a 5): "))
+cod_estado = int(input("Digite o código do estado (1 a 5): "))
 peso_carga_tone = float(input("Digite o peso da carga do caminhão em toneladas: "))
-cod_carga = int(input("Digite o código da carga (10 a 40"))
+cod_carga = int(input("Digite o código da carga (10 a 40): "))
 peso_carga_kg = peso_carga_tone * 1000
-preco_carga = 0
-preco = 0
-imposto_estado = 0
 imposto = 0
-valor_total = 0
+preco_por_kg = 0
+
 if cod_estado ==1:
-    imposto_estado = 35
-    if 10 <= cod_carga <20:
-        preco = 100
-    elif 20 <= cod_carga <30:
-        preco = 250
-    elif 30 <= cod_carga <40:
-        preco = 340
-    preco_carga = preco *peso_carga_kg
-    imposto = preco_carga * (imposto_estado /100)
-    valor_total = preco_carga - imposto
+    imposto = 35
 elif cod_estado == 2:
     imposto = 25
+elif cod_estado == 3:
+    imposto = 15
+elif cod_estado ==4:
+    imposto = 5
+else:
+    imposto = 0
 
-    '''
+
+if 10 <= cod_carga <=20:
+    preco_por_kg =100
+elif 21 <= cod_carga  <=30:
+    preco_por_kg = 250
+elif 31 <= cod_carga <=40:
+    preco_por_kg = 340
+
+preco_carga = preco_por_kg * peso_carga_kg
 
 
-
+print(f"O peso da carga do caminhão convertido em quilos é igual a: {peso_carga_kg} kg.")
+print (f"O preço da carga do caminhão é de: R${preco_carga:.2f}")
+print (f"O percentual do imposto foi de {imposto}%, totalizando R${preco_carga*(imposto/100):.2f} de imposto.")
+print (f"O valor total transportardo foi de: R${preco_carga + (preco_carga*(imposto/100)):.2f}")
---
+++ aula03-modulos-condicionais/exercicio_10.py
@@ -0,0 +1,21 @@
+print("A seguir digite os lados de um triângulo em ordem decrescente.")
+
+lado_a = float(input("Digite o lado A de um triângulo: "))
+lado_b = float(input("Digite o lado B de um triângulo: "))
+lado_c = float(input("Digite o lado C de um triângulo: "))
+
+if lado_a >= lado_b + lado_c:
+    print("Os valores dos lados digitados não formam um triângulo!")
+else:
+    if lado_a == lado_b == lado_c:
+        print("Triângulo equilátero;")
+    elif lado_a == lado_b or lado_b == lado_c or lado_c == lado_a:
+        print("Triângulo isósceles;")
+    else:
+        if lado_a**2 == lado_b**2 + lado_c**2:
+            print("Triângulo retângulo;")
+        elif lado_a**2 > lado_b**2 + lado_c**2:
+            print("Triângulo obtusângulo;")
+        elif lado_a**2 < lado_b**2 + lado_c**2:
+            print("Triângulo acutângulo;")
+
