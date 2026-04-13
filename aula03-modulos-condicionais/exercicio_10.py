9d520f3db9a34312d43ee60e90ac9930c5b73813
marcospeterson <marcospetersonmartins@gmail.com>
2026-04-13 11:38:57 -0300

    corrijindo exercicio

--- aula03-modulos-condicionais/exercicio_10.py
+++ aula03-modulos-condicionais/exercicio_10.py
@@ -7,15 +7,16 @@ lado_c = float(input("Digite o lado C de um triângulo: "))
 if lado_a >= lado_b + lado_c:
     print("Os valores dos lados digitados não formam um triângulo!")
 else:
-    if lado_a == lado_b == lado_c:
-        print("Triângulo equilátero;")
-    elif lado_a == lado_b or lado_b == lado_c or lado_c == lado_a:
-        print("Triângulo isósceles;")
-    else:
         if lado_a**2 == lado_b**2 + lado_c**2:
             print("Triângulo retângulo;")
         elif lado_a**2 > lado_b**2 + lado_c**2:
             print("Triângulo obtusângulo;")
         elif lado_a**2 < lado_b**2 + lado_c**2:
             print("Triângulo acutângulo;")
+
+
+if lado_a == lado_b == lado_c:
+    print("Triângulo equilátero;")
+elif lado_a == lado_b or lado_b == lado_c or lado_c == lado_a:
+    print("Triângulo isósceles;")
 
